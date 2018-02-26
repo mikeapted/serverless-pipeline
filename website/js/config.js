b48593fd05b0fdf859992b549c717e4197ae3531
Mike Apted <mikapted@amazon.com>
2018-02-26 11:30:34 -0500

    Config update

--- website/js/config.js
+++ website/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_LrYgFrbUK', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3276k7ia31rtvmcsjqdbog2v9t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_Hh98u1o9t	', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1cbm4j1v6crqqvtolg4dp4sabr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://ec4m45wank.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://ho3eimzfj9.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
