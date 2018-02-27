363c9ba234ed090a600a0a6267b961379ef3737e
Mike Apted <mikapted@amazon.com>
2018-02-27 07:27:48 -0500

    Fix back to one file

--- index.js
+++ index.js
@@ -55,6 +55,37 @@ function emitLambdaAge() {
     console.log('Lambda is ' + daysOld + ' days old!');
 }
 
+exports.bgcolour = (event, context, callback) => {
+
+  var originURL = process.env.ORIGIN_URL || '*';
+
+  /*
+  if (!event.requestContext.authorizer) {
+    errorResponse('Authorization not configured', context.awsRequestId, callback);
+    return;
+  }
+  const username = event.requestContext.authorizer.claims['cognito:username'];
+  */
+
+  emitLambdaAge();
+
+  // This variable can be updated and checked in to your repository
+  // to update the number of SAM squirrels on the screen.
+  var samColor = process.env.BG_COLOUR || '#967da7';
+
+  console.log('The background colour: ' + samColor);
+
+  callback(null, {
+      "statusCode": 200,
+      "body": samColor,
+      "headers":
+      {
+          "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+          "Access-Control-Allow-Methods": "GET,OPTIONS",
+          "Access-Control-Allow-Origin": originURL
+      }
+  });
+}
 
 /*
 function errorResponse(errorMessage, awsRequestId, callback) {
